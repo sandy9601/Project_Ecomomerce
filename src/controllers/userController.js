c2bd987f2e1ed74e74f1c808a63e941147f01f5c
unknown <manishabharti236@gmail.com>
2022-07-25 15:33:20 +0530

    project-5

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -17,4 +17,22 @@ data.password=hash // * assigning dcrypted password
 const createUser=await userModel.create(data)
 res.status(201).send({status:true,message:"User created successfully",data:createUser})
 }
-module.exports={userCreate}
+
+//...................................................getapi.............................................................//
+const getapi =async function(req,res){
+    try{
+    let userId=req.param.userId
+    let user=await userModel.findOne(userId)
+    res.status(200).send({status: true, data:user})
+
+}catch(err){
+    res.status(500).send({status:false, msg: err.message})
+}
+}
+
+
+
+
+
+
+module.exports={userCreate,getapi}
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const {awsApi}=require("../middlewares/aws")
-const{userCreate}=require("../controllers/userController")
+const{userCreate, getapi}=require("../controllers/userController")
 
 module.exports = router;
 
 router.post("/register",awsApi,userCreate);
-
+router.get("/user/:userId",getapi);
 
 
