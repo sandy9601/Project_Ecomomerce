e7eab15e669599bb454666c1a6b0ccfdf172d49e
sandy9601 <darshanamsandeep375@gmail.com>
2022-07-25 12:04:08 +0530

    boiler plate

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const route = require("./routes/route");
+const app=express()
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+const multer= require("multer");
+
+app.use( multer().any())
+
+mongoose
+  .connect(
+    "mongodb+srv://SandeepDarshanam:9866203258Aa@cluster0.pr0hn.mongodb.net/group31Database?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+    }
+  )
+
+  .then(() => console.log("MongoDb is connected"))
+  .catch((err) => console.log(err));
+
+app.use("/", route);
+
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Express app running on port " + (process.env.PORT || 3000));
+});
+
