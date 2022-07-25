1a6c4070f90ec3068008ae866f46d3c8130da2a0
sandy9601 <darshanamsandeep375@gmail.com>
2022-07-25 19:16:05 +0530

    update api

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const userModel = require("../models/userModel");
 const bcrypt = require("bcrypt");
 const JWT = require("jsonwebtoken");
-const secretkey="Products Management"
+const secretkey = "Products Management";
 
 const isValid = function (value) {
   if (typeof value !== "string" || value === null) return false;
@@ -30,12 +30,10 @@ const userCreate = async function (req, res) {
         .send({ status: false, message: "fname is required" });
     }
     if (!isValidName(fname)) {
-      return res
-        .status(400)
-        .send({
-          status: false,
-          message: `${fname} is not a valid formate  for fname`,
-        });
+      return res.status(400).send({
+        status: false,
+        message: `${fname} is not a valid formate  for fname`,
+      });
     }
 
     // * lnameValidation
@@ -46,12 +44,10 @@ const userCreate = async function (req, res) {
         .send({ status: false, message: "lname is required" });
     }
     if (!isValidName(lname)) {
-      return res
-        .status(400)
-        .send({
-          status: false,
-          message: `${lname} is not a valid formate  for lname`,
-        });
+      return res.status(400).send({
+        status: false,
+        message: `${lname} is not a valid formate  for lname`,
+      });
     }
 
     // * emailValidation
@@ -63,22 +59,18 @@ const userCreate = async function (req, res) {
     }
 
     if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
-      return res
-        .status(400)
-        .send({
-          status: false,
-          message: `${email} is not a valid formate  for email`,
-        });
+      return res.status(400).send({
+        status: false,
+        message: `${email} is not a valid formate  for email`,
+      });
     }
 
     const checkEmail = await userModel.findOne({ email: email });
     if (checkEmail) {
-      return res
-        .status(400)
-        .send({
-          status: false,
-          message: `${email} is already used please use another email`,
-        });
+      return res.status(400).send({
+        status: false,
+        message: `${email} is already used please use another email`,
+      });
     }
     // * phoneValidation
     if (!isValid(phone)) {
@@ -88,35 +80,31 @@ const userCreate = async function (req, res) {
     }
 
     if (!/^(?:(?:\+|0{0,2})91(\s*[\-]\s*)?|[0]?)?[6789]\d{9}$/.test(phone)) {
-      return res
-        .status(400)
-        .send({
-          status: false,
-          message: `${phone} is invalid please enter a valid phone number`,
-        });
+      return res.status(400).send({
+        status: false,
+        message: `${phone} is invalid please enter a valid phone number`,
+      });
     }
     const checkPhone = await userModel.findOne({ phone: phone });
     if (checkPhone) {
-      return res
-        .status(400)
-        .send({
-          status: false,
-          message: `${phone} is already used please use another phoneNumber`,
-        });
+      return res.status(400).send({
+        status: false,
+        message: `${phone} is already used please use another phoneNumber`,
+      });
     }
+
     // * passwordValidation
+
     if (!isValid(password)) {
       return res
         .status(400)
         .send({ status: false, message: "password is required" });
     }
     if (password.length < 8 || password.length > 15) {
-      return res
-        .status(400)
-        .send({
-          status: false,
-          message: "password must contains atleast 8 charcaters upto 15",
-        });
+      return res.status(400).send({
+        status: false,
+        message: "password must contains atleast 8 charcaters upto 15",
+      });
     }
     //* password decrypt
     var saltRounds = 10;
@@ -124,7 +112,7 @@ const userCreate = async function (req, res) {
     data.password = hash; // * assigning dcrypted password
 
     // *addressValidation
-    
+
     if (!isValid(address)) {
       return res
         .status(400)
@@ -143,12 +131,10 @@ const userCreate = async function (req, res) {
         .send({ status: false, message: "street is required in address" });
     }
     if (!isValidUserDetails(street)) {
-      return res
-        .status(400)
-        .send({
-          status: false,
-          message: `${street} is inavlid formate for street`,
-        });
+      return res.status(400).send({
+        status: false,
+        message: `${street} is inavlid formate for street`,
+      });
     }
 
     //* cityValidation
@@ -159,12 +145,10 @@ const userCreate = async function (req, res) {
         .send({ status: false, message: "city is required" });
     }
     if (!isValidName(city)) {
-      return res
-        .status(400)
-        .send({
-          status: false,
-          message: `${city} is not a valid formate  for city`,
-        });
+      return res.status(400).send({
+        status: false,
+        message: `${city} is not a valid formate  for city`,
+      });
     }
 
     // * pincodeValidation
@@ -175,22 +159,18 @@ const userCreate = async function (req, res) {
         .send({ status: false, message: "pincode is required" });
     }
     if (!isValidName(city)) {
-      return res
-        .status(400)
-        .send({
-          status: false,
-          message: `${pincode} is not a valid formate  for city`,
-        });
+      return res.status(400).send({
+        status: false,
+        message: `${pincode} is not a valid formate  for city`,
+      });
     }
 
     const createUser = await userModel.create(data);
-    res
-      .status(201)
-      .send({
-        status: true,
-        message: "User created successfully",
-        data: createUser,
-      });
+    res.status(201).send({
+      status: true,
+      message: "User created successfully",
+      data: createUser,
+    });
   } catch (error) {
     res.status(500).send({ status: false, error: error.message });
   }
@@ -201,15 +181,22 @@ const userCreate = async function (req, res) {
 const getapi = async function (req, res) {
   try {
     let userId = req.params.userId;
-    token=req.headers.authorization.split(" ")[1]
-    let decoded= JWT.verify(token,secretkey)
-    const validUserId=decoded.userId
-    console.log(userId,validUserId)
-    if(validUserId!=userId){
-        return res.status(401).send({status:false,message:"user don't have permission to get details"})
+    token = req.headers.authorization.split(" ")[1];
+    let decoded = JWT.verify(token, secretkey);
+    const validUserId = decoded.userId;
+    console.log(userId, validUserId);
+    if (validUserId != userId) {
+      return res
+        .status(401)
+        .send({
+          status: false,
+          message: "user don't have permission to get details",
+        });
     }
-    let user = await userModel.findOne({_id:userId});
-    res.status(200).send({ status: true,message: "User profile details", data: user });
+    let user = await userModel.findOne({ _id: userId });
+    res
+      .status(200)
+      .send({ status: true, message: "User profile details", data: user });
   } catch (err) {
     res.status(500).send({ status: false, error: err.message });
   }
@@ -219,7 +206,7 @@ const getapi = async function (req, res) {
 
 const logInUser = async function (req, res) {
   try {
- const {email,password}=req.body
+    const { email, password } = req.body;
     if (!email)
       return res
         .status(400)
@@ -231,9 +218,7 @@ const logInUser = async function (req, res) {
 
     const check = await userModel.findOne({ email: email });
     if (!check)
-      return res
-        .status(400)
-        .send({ status: false, msg: "incorrect email" });
+      return res.status(400).send({ status: false, msg: "incorrect email" });
     const passwordcheck = bcrypt.compareSync(password, check.password); // true
     if (!passwordcheck)
       return res
@@ -247,10 +232,141 @@ const logInUser = async function (req, res) {
       secretkey
     );
     res.setHeader("x-api-key", token);
-    res.status(200).send({ status: true, message: "User login successfull", data: {userId:check._id,token:token} });
+    res
+      .status(200)
+      .send({
+        status: true,
+        message: "User login successfull",
+        data: { userId: check._id, token: token },
+      });
   } catch (err) {
     res.status(500).send({ status: false, msg: err.message });
   }
 };
 
-module.exports = { userCreate, getapi, logInUser };
+
+
+//*...................................................updatenApi.............................................................//
+
+
+let validName = /^[a-zA-Z ]{3,30}$/
+let validPass = /^[a-zA-Z0-9@*&]{8,15}$/
+
+const updateUser = async function (req, res) {
+    let userId = req.params.userId
+    let updateData = req.body
+    const { fname, lname, email, phone, password, address, profileImage } = updateData
+    let final = {}
+
+    if (Object.keys(updateData).length == 0) {
+        return res.status(400).send({ status: false, message: "Body Should Not Empty" })
+    }
+
+    if (fname) {
+        if (!isValid(fname) || !validName.test(fname)) {
+            return res.status(400).send({ status: false, message: "first Name is not given or invalid firstName" })
+        }
+        final.fname = fname
+    }
+
+    if (lname) {
+        if (!isValid(lname) || !validName.test(lname)) {
+            return res.status(400).send({ status: false, message: "last Name is not given or invalid lastName" })
+        }
+        final.lname = lname
+    }
+
+    if (email) {
+        if (!isValid(email)) {
+            return res.status(400).send({ status: false, message: "Email is required" });
+
+        }
+        if (!validator.isEmail(email)) {
+            return res.status(400).send({ status: false, message: "Enter a valid email" })
+        }
+        let uniqueEmail = await userModel.findOne({ email: userDetails.email });
+        if (uniqueEmail) {
+            return res.status(400).send({ status: false, message: "Email  already Used" })
+        }
+        final.email = email
+    }
+
+    if (profileImage) {
+        final.profileImage = profileImage
+    }
+
+    if (phone) {
+        if (!isValid(phone)) {
+            res.status(400).send({ status: false, message: "Phone number is required" });
+            return
+        }
+        if (!/^[6-9]{1}[0-9]{9}$/.test(phone)) {
+            res.status(400).send({ status: false, message: "Please provide valid phone number" });
+            return;
+        }
+        let uniquePhone = await userModel.findOne({ phone: userDetails.phone });
+        if (uniquePhone) {
+            return res.status(400).send({ status: false, message: "Phone no. already Used" })
+        }
+        final.phone = phone
+    }
+
+    if (password) {
+        if (!isValid(password)) {
+            return res.status(400).send({ status: false, message: "Password is required " })
+        }
+        if (!validPass.test(password)) {
+            return res.status(400).send({ status: false, message: "Password should be min 8 and max length 15" })
+        }
+        const saltRounds = 10;
+        const hash = bcrypt.hashSync(password, saltRounds);
+        final.password = hash
+
+    }
+
+    if (address) {
+
+        const newAddress = JSON.parse(address)
+        final.address = newAddress
+
+        if (newAddress.shipping) {
+            if (!isValid(newAddress.shipping.street)) {
+                return res.status(400).send({ status: false, message: "please provide the street" })
+            }
+
+            if (!isValid(newAddress.shipping.city)) {
+                return res.status(400).send({ status: false, message: "please provide the city" })
+            }
+
+            if (!newAddress.shipping.pincode || typeof (newAddress.shipping.pincode) != "number") {
+                return res.status(400).send({ status: false, message: "pincode is not given OR it is not is proper format" })
+            }
+
+        }
+
+        if (newAddress.billing) {
+            if (!isValid(newAddress.billing.street)) {
+                return res.status(400).send({ status: false, message: "please provide the street" })
+            }
+
+            if (!isValid(newAddress.billing.city)) {
+                return res.status(400).send({ status: false, message: "please provide the city" })
+            }
+
+            if (!newAddress.billing.pincode || typeof (newAddress.billing.pincode) != "number") {
+                return res.status(400).send({ status: false, message: "pincode is not given OR it is not is proper format" })
+            }
+        }
+    }
+
+    const updateResult = await userModel.findOneAndUpdate({ _id: userId }, final, { new: true })
+    return res.status(200).send({ status: true, message: "succesfully Updated", Data: updateResult })
+}
+
+
+
+
+
+
+
+module.exports = { userCreate, getapi, logInUser,updateUser};
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const {awsApi}=require("../middlewares/aws")
-const{userCreate, getapi,logInUser}=require("../controllers/userController")
+const{userCreate, getapi,logInUser,updateUser}=require("../controllers/userController")
 
 module.exports = router;
 
 router.post("/register",awsApi,userCreate);
 router.get("/user/:userId/profile",getapi);
 router.post("/login",logInUser)
+router.put("/user/:userId/profile",updateUser)
+
 
 
